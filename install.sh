caa407503778865b362aabe5ca564e41ec45fee4
Florian Mros <mros.florian@gmail.com>
2023-02-13 20:22:43 +0100

    te

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# install yay (AUR helper)
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
 yay -S hyprland-bin polkit-gnome ffmpeg neovim viewnior       \
 rofi pavucontrol thunar starship wl-clipboard wf-recorder     \
 swaybg grimblast-git ffmpegthumbnailer tumbler playerctl      \
