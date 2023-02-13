0ef519f15649f9643ea6923391bfb25ff44a6fd1
Florian Mros <mros.florian@gmail.com>
2023-02-13 20:15:08 +0100

    sets

--- install.sh
+++ install.sh
@@ -6,3 +6,11 @@ swaybg grimblast-git ffmpegthumbnailer tumbler playerctl      \
 noise-suppression-for-voice thunar-archive-plugin kitty       \
 waybar-hyprland wlogout swaylock-effects sddm-git pamixer     \
 nwg-look-bin nordic-theme papirus-icon-theme dunst
+
+
+echo "[Autologin]" | sudo tee /etc/sddm.conf
+echo "User=floork" | sudo tee -a /etc/sddm.conf
+echo "Session=hyprland" | sudo tee -a /etc/sddm.conf
+
+sudo groupadd autologin
+sudo usermod -aG autologin floork
