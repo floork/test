085c421551abcfb67ffd7465b49fa71dd715de74
Florian Mros <mros.florian@gmail.com>
2023-02-13 20:36:02 +0100

    sef

--- install.sh
+++ install.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-# install yay (AUR helper)
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-
-yay -S hyprland-bin polkit-gnome ffmpeg neovim viewnior       \
-rofi pavucontrol thunar starship wl-clipboard wf-recorder     \
-swaybg grimblast-git ffmpegthumbnailer tumbler playerctl      \
-noise-suppression-for-voice thunar-archive-plugin kitty       \
-waybar-hyprland wlogout swaylock-effects sddm-git pamixer     \
-nwg-look-bin nordic-theme papirus-icon-theme dunst
-
-
-echo "[Autologin]" | sudo tee /etc/sddm.conf
-echo "User=floork" | sudo tee -a /etc/sddm.conf
-echo "Session=hyprland" | sudo tee -a /etc/sddm.conf
-
-sudo groupadd autologin
-sudo usermod -aG autologin floork
+yay -S --needed --noconfirm hyprland-bin polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst
