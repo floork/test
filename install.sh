5bd71d5152c10f6d5ea1b7417bf9511fb6ce116b
Florian Mros <mros.florian@gmail.com>
2023-02-14 17:53:49 +0100

    sfa

--- install.sh
+++ install.sh
@@ -12,6 +12,11 @@ instyay(){
 install(){
 yay -S --needed --noconfirm hyprland-bin polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst
 }
-
-instyay
+inst(){
+  git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+sudo make install
+}
+#instyay
+inst
 
