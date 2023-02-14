ab712d24cba3e6e1671559c3f199a40ecf82e227
Florian Mros <mros.florian@gmail.com>
2023-02-14 17:41:22 +0100

    lsadfjk

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+instyay(){
+  # install yay (AUR helper)
+            sudo pacman -Syu --noconfirm
+            sudo pacman -S --noconfirm --needed git base-devel
+            git clone https://aur.archlinux.org/yay-bin.git
+            cd yay-bin
+            makepkg -si
+}
+
+install(){
 yay -S --needed --noconfirm hyprland-bin polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst
+}
+
+instyay
+
