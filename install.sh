c46980b0425d453c2408be978194993a188abdf2
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-02-23 17:30:15 +0100

    test

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 inst_docker(){
-    sudo apt update -y && sudo apt upgrade -y
-    curl -sSL https://get.docker.com | sh
+    apt-get remove docker docker-engine docker.io containerd runc
+    apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    apt update
+    apt-get install docker-ce docker-ce-cli containerd.io
+    
 }
 
 conf_docker(){
-  sudo usermod -aG docker lepotato
-  echo "you will be loged out in 2 seconds, please log back in to apply changes"
-  sleep 2s
-  logout
-# after logout run
-# docker run hello-world
+    sudo usermod -aG docker lepotato
+    echo "you will be loged out in 2 seconds, please log back in to apply changes"
+    sleep 2s
+    logout
+    # after logout run
+    # docker run hello-world
 }
 inst_docker
 conf_docker
