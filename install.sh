564438b9c47add4bf01d6a7de6de17cf1abc0fa2
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-02-23 17:37:06 +0100

    test

--- install.sh
+++ install.sh
@@ -7,17 +7,7 @@ inst_docker(){
     add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
     apt update
     apt-get install docker-ce docker-ce-cli containerd.io
-    
+
 }
 
-conf_docker(){
-    sudo usermod -aG docker lepotato
-    echo "you will be loged out in 2 seconds, please log back in to apply changes"
-    sleep 2s
-    logout
-    # after logout run
-    # docker run hello-world
-}
 inst_docker
-conf_docker
-
